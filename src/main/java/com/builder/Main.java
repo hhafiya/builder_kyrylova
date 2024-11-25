5fd807feecc85cc726196b9e954f0f6b89758157
hhafiya <149326005+hhafiya@users.noreply.github.com>
2024-11-25 23:14:48 +0200

    Update Main.java

--- src/main/java/com/builder/Main.java
+++ src/main/java/com/builder/Main.java
@@ -1,20 +1,27 @@
 package com.builder;
 
 import java.util.Arrays;
-
 import com.builder.task1.Student;
 
 public class Main {
+    private static final int GRADE_ONE = 85;
+    private static final int GRADE_TWO = 90;
+    private static final int GRADE_THREE = 78;
+    private static final int FINAL_GRADE = 10;
+    private static final int STUDENT_AGE = 20;
+    private static final int STUDENT_HEIGHT = 175;
+
     public static void main(String[] args) {
         Student student = Student.builder()
-        .firstName("John")
-        .lastName("Doe")
-        .grades(Arrays.asList(85, 90, 78))
-        .grade(10)
-        .group("A1")
-        .age(20)
-        .height(175)
-        .build();
-    System.out.println(student);
+                .firstName("John")
+                .lastName("Doe")
+                .grades(Arrays.asList(GRADE_ONE, GRADE_TWO, GRADE_THREE))
+                .grade(FINAL_GRADE)
+                .group("A1")
+                .age(STUDENT_AGE)
+                .height(STUDENT_HEIGHT)
+                .build();
+
+        System.out.println(student);
     }
 }
